3044b025d95c23b96f04207bb9ae23ebe71dc485
remyogasawara <remyogasawara@gmail.com>
2022-07-21 21:57:26 -0700

    week 6 changes

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,20 @@ const express = require("express"),
 const bodyParser = require('body-parser');
 const fs = require("fs");
 
-app.use(cors());
+const basicAuth = require("express-basic-auth");
+var { authenticator, upsertUser, cookieAuth } = require("./authentication");
+const auth = basicAuth({
+    authorizer: authenticator
+});
+const cookieParser = require("cookie-parser");
+app.use(cookieParser("82e4e438a0705fabf61f9854e3b575af"));
+
+app.use(cors({
+    credentials: true,
+    origin: 'http://localhost:3000'
+}));
+// app.use(cors());
+
 app.use(bodyParser.json({ extended: true }));
 app.listen(port, () => console.log("Backend server live on " + port));
 
@@ -14,7 +27,7 @@ app.get("/", (req, res) => {
     });
 
 //add new item to json file
-app.post("/items", addItem)
+app.post("/items", cookieAuth, addItem)
 
 function addItem (request, response) {
     // Converting Javascript object (Task Item) to a JSON string
@@ -39,7 +52,7 @@ function addItem (request, response) {
     response.send(200)
     }
 
-app.get("/items", getItems)
+app.get("/items", cookieAuth, getItems)
 //** week5, get all items from the json database*/
   function getItems (request, response) {
     var data = fs.readFileSync('database.json');
@@ -51,7 +64,7 @@ app.get("/items", getItems)
     // Note this won't work, why? response.send();
   } 
 
-app.get("/items/search",searchItems)
+app.get("/items/search", cookieAuth, searchItems)
 //**week 5, search items service */
   function searchItems (request, response) {
     var searchField = request.query.taskname;
@@ -65,3 +78,21 @@ app.get("/items/search",searchItems)
     //console.log(returnData);
     response.json(returnData);
   }
+
+app.get("/authenticate", auth, (req, res) => {
+    console.log(`user logging in: ${req.auth.user}`);
+    res.cookie('user', req.auth.user, { signed: true });
+    res.sendStatus(200);
+});
+
+app.post("/users", (req, res) => {
+    const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
+    const [username, password] = Buffer.from(b64auth, 'base64').toString().split(':')
+    const upsertSucceeded = upsertUser(username, password)
+    res.sendStatus(upsertSucceeded ? 200 : 401);
+});
+
+app.get("/logout", (req, res) => {
+    res.clearCookie('user');
+    res.end();
+});
